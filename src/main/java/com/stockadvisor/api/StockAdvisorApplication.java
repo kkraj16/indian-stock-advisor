3852e85616963ffd72ef36c0ded1f8e2e9b7eacf
K.K Rajpurohit <165471158+kkraj16@users.noreply.github.com>
2025-04-18 12:40:46 +0530

    create project basic skelton 1. Setup springboot project

---
+++ src/main/java/com/stockadvisor/api/StockAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.stockadvisor.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockadvisor/api/StockAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockadvisor.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
